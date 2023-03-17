9890246855c3266b428fe16fdddf378abaa03a70
Hendri Mardani <49816104+hendrimardani@users.noreply.github.com>
2023-03-17 22:20:33 +0700

    Update scraping.py

--- scraping.py
+++ scraping.py
@@ -1,4 +1,3 @@
-## ----  Cek Respons web server ---- , Jika tidak ada error aktifkan komentar yang bawah
 from pathlib import Path
 
 import scrapy
@@ -16,39 +15,19 @@ class QuotesSpider(scrapy.Spider):
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        print(response.url)
-
-
-
-
-# from pathlib import Path
-
-# import scrapy
-
-
-# class QuotesSpider(scrapy.Spider):
-#     name = "quotes"
-
-#     def start_requests(self):
-#         urls = [
-#             'https://top-1000-sekolah.ltmpt.ac.id/?page=1&per-page=100',
-#             'https://top-1000-sekolah.ltmpt.ac.id/?page=2&per-page=100',
-#         ]
-#         for url in urls:
-#             yield scrapy.Request(url=url, callback=self.parse)
-
-#     def parse(self, response):
-#         yield {
-#             "npsn":response.css("#w0 > table > tbody > tr:nth-child(1) > td:nth-child(3) \
-#                                 ::text").extract(),
-#             "nama_sekolah":response.css("#w0 > table > tbody > tr:nth-child(1) > td:nth-child(4) \
-#                                         ::text").extract(),
-#             "nilai_total":response.css("#w0 > table > tbody > tr:nth-child(1) > td:nth-child(5) \
-#                                        ::text").extract(),
-#             "provinsi":response.css("#w0 > table > tbody > tr:nth-child(1) > td:nth-child(6) \
-#                                     ::text").extract(),
-#             "kota/kab":response.css("#w0 > table > tbody > tr:nth-child(1) > td:nth-child(7) \
-#                                     ::text").extract(),
-#             "jenis_sekolah":response.css("#w0 > table > tbody > tr:nth-child(1) > td:nth-child(8) \
-#                                          ::text").extract(),  
-#         }
+        # print(response.url) # Cek apakah ada error atau tidak
+        
+        yield {
+            "npsn":response.css("#w0 > table > tbody > tr:nth-child(1) > td:nth-child(3) \
+                                ::text").extract(),
+            "nama_sekolah":response.css("#w0 > table > tbody > tr:nth-child(1) > td:nth-child(4) \
+                                        ::text").extract(),
+            "nilai_total":response.css("#w0 > table > tbody > tr:nth-child(1) > td:nth-child(5) \
+                                       ::text").extract(),
+            "provinsi":response.css("#w0 > table > tbody > tr:nth-child(1) > td:nth-child(6) \
+                                    ::text").extract(),
+            "kota/kab":response.css("#w0 > table > tbody > tr:nth-child(1) > td:nth-child(7) \
+                                    ::text").extract(),
+            "jenis_sekolah":response.css("#w0 > table > tbody > tr:nth-child(1) > td:nth-child(8) \
+                                         ::text").extract(),  
+        }
