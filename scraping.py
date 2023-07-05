695067c4610cc9ae04faba996f1b88e250afa249
Hendri Mardani <49816104+hendrimardani@users.noreply.github.com>
2023-07-05 00:37:41 -0400

    Update scraping.py

--- scraping.py
+++ scraping.py
@@ -109,3 +109,56 @@ class QuotesSpider(scrapy.Spider):
 #                     "jenis_sekolah":sekolah.css(f"tr:nth-child({str(x)}) > td:nth-child(8) \
 #                                                 ::text").extract(),
 #                 }
+
+
+
+
+
+# # Fungsi try-except => Jika program try (error) maka except akan dijalankan
+# from pathlib import Path
+
+# import scrapy
+
+
+# class QuotesSpider(scrapy.Spider):
+#     name = "quotes"
+
+#     def start_requests(self):
+#         urls = [
+#             'https://top-1000-sekolah.ltmpt.ac.id/?page=1&per-page=100',
+#             'https://top-1000-sekolah.ltmpt.ac.id/?page=2&per-page=100',
+#         ]
+#         for url in urls:
+#             yield scrapy.Request(url=url, callback=self.parse)
+
+#     def parse(self, response):
+#         try:
+#             yield {
+#                 "npsn":response.css("#w0 > table > tbody > tr:nth-child(1) > td:nth-child(3) \
+#                                     ::text").extract(),
+#                 "nama_sekolah":response.css("#w0 > table > tbody > tr:nth-child(1) > td:nth-child(4) \
+#                                             ::text").extract(),
+#                 "nilai_total":response.css("#w0 > table > tbody > tr:nth-child(1) > td:nth-child(5) \
+#                                         ::text").extract(),
+#                 "provinsi":response.css("#w0 > table > tbody > tr:nth-child(1) > td:nth-child(6) \
+#                                         ::text").extract(),
+#                 "kota/kab":response.css("#w0 > table > tbody > tr:nth-child(1) > td:nth-child(7) \
+#                                         ::text").extract(),
+#                 "jenis_sekolah":response.css("#w0 > table > tbody >tr:nth-child(1) > td:nth-child(8) \
+#                                             ::text").extract(),
+#                 }
+#         except:
+#             yield {
+#                 "npsn":response.css("#w0 > table > tbody > tr:nth-child(1) > td:nth-child(3) \
+#                                     ::text").extract(),
+#                 "nama_sekolah":response.css("#w0 > table > tbody > tr:nth-child(1) > td:nth-child(4) \
+#                                             ::text").extract(),
+#                 "nilai_total":response.css("#w0 > table > tbody > tr:nth-child(1) > td:nth-child(5) \
+#                                         ::text").extract(),
+#                 "provinsi":response.css("#w0 > table > tbody > tr:nth-child(1) > td:nth-child(6) \
+#                                         ::text").extract(),
+#                 "kota/kab":response.css("#w0 > table > tbody > tr:nth-child(1) > td:nth-child(7) \
+#                                         ::text").extract(),
+#                 "jenis_sekolah":response.css("#w0 > table > tbody >tr:nth-child(1) > td:nth-child(8) \
+#                                             ::text").extract(),
+#                 }
